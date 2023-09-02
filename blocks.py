f45ff143baf8209482c4f27dce49a5273e39b2fb
GreevesW <138601558+GreevesW@users.noreply.github.com>
2023-09-02 18:51:47 +0100

    Update blocks.py
    Comments added

--- blocks.py
+++ blocks.py
@@ -1,21 +1,11 @@
 
-#Defines all of the coordinates for each of the rotational states for LBlocks
-#Positions refere to coordinates in a grid that link to an objects rotational state
-
-        #makes all blocks appear at the centre of the screen by default
-        #the I block must appear 1 cell above the other blocks as its centre of rotation is at the top of the block and so leaves a gap by default.
-
-
-
-#The O block only has one rotational state as it looks the same no matter how it is rotated
-#The O block needs to be centred one cell to the right when compared to other blocks
-
-
 #defines the rotation positions for all block types
 
 from block import Block
 from position import Position
 
+#Defines all of the coordinates for each of the rotational states for LBlocks
+#Positions refere to coordinates in a grid that link to an objects rotational state
 class LBlock(Block):
 	def __init__(self):
 		super().__init__(id = 1)
@@ -26,6 +16,7 @@ class LBlock(Block):
 			3: [Position(0, 0), Position(0, 1), Position(1, 1), Position(2, 1)]
 		}
 		self.move(0, 3)
+		#makes all blocks appear at the centre of the screen by default
 
 class JBlock(Block):
     def __init__(self):
@@ -48,6 +39,7 @@ class IBlock(Block):
             3: [Position(0, 1), Position(1, 1), Position(2, 1), Position(3, 1)]
         }
         self.move(-1, 3)
+	#the I block must appear 1 cell above the other blocks as its centre of rotation is at the top of the block and so leaves a gap by default.
 
 class OBlock(Block):
     def __init__(self):
@@ -56,6 +48,9 @@ class OBlock(Block):
             0: [Position(0, 0), Position(0, 1), Position(1, 0), Position(1, 1)]
         }
         self.move(0, 4)
+	#The O block only has one rotational state as it looks the same no matter how it is rotated
+	#The O block needs to be centred one cell to the right when compared to other blocks
+
 
 class SBlock(Block):
     def __init__(self):
